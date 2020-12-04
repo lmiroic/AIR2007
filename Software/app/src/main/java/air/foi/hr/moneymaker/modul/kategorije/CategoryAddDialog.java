29d929d93b20081c08979b372b0042eea5401aec
Luka Miroić <lmiroic@foi.hr>
2020-12-04 13:30:09 +0100

    [AIR2007-64] Implementacija brisanja odabrane kategorije transakcije

--- Software/app/src/main/java/air/foi/hr/moneymaker/modul/kategorije/CategoryAddDialog.java
+++ Software/app/src/main/java/air/foi/hr/moneymaker/modul/kategorije/CategoryAddDialog.java
@@ -34,15 +34,22 @@ import air.foi.hr.moneymaker.manager.CustomAdapterKategorije;
 public class CategoryAddDialog extends Dialog implements View.OnClickListener {
     private Context context;
     public int tipTroska=0;
-    private Button button;
+    private Button button,button2;
     private EditText nazivKategorije;
     private RecyclerView recyclerView;
     public Spinner spinner;
     private OnDialogCategoryResult onDialogCategoryResult;
     private ArrayList<CategoryAddModel> arrayList;
     private CustomAdapterKategorije customAdapterKategorije;
+    private KategorijaTransakcije kategorijaTransakcije;
+    public  String ikone[] = {"ic_book","ic_fitness","ic_gaming","ic_kucni_ljubimci","ic_lijekovi","ic_obrazovanje","ic_pokloni","ic_travelling","ic_bank","ic_coffee","ic_growth","ic_car_service","ic_money_transfer","ic_life_insurance","ic_gorivo","ic_hrana","ic_placa","ic_odjeca","ic_rezije"};
     public CategoryAddDialog(@NonNull Context context) {
         super(context);
+    }
+
+    public CategoryAddDialog(@NonNull Context context, KategorijaTransakcije kategorijaTransakcije) {
+        super(context);
+        this.kategorijaTransakcije = kategorijaTransakcije;
     }
 
     @Override
@@ -51,11 +58,34 @@ public class CategoryAddDialog extends Dialog implements View.OnClickListener {
         requestWindowFeature(Window.FEATURE_NO_TITLE);
         setContentView(R.layout.dialog_kategorije);
         button=findViewById(R.id.btnDialogKategorije);
+        button2=findViewById(R.id.btnDeleteKategorije);
+        button2.setVisibility(View.GONE);
         spinner=findViewById(R.id.cmboxTipTransakcije);
         nazivKategorije=findViewById(R.id.editTextDialogText);
         PostaviRecycleView();
         button.setOnClickListener(this);
         PostaviSpinner();
+        if(kategorijaTransakcije!=null){
+            nazivKategorije.setText(kategorijaTransakcije.getNaziv());
+            spinner.setSelection(kategorijaTransakcije.getTipTransakcije()-1);
+            oznaciKategoriju(kategorijaTransakcije.getIkonaKategorije());
+            button.setText("Ažuriraj");
+            button.setOnClickListener(this);
+            button2.setVisibility(View.VISIBLE);
+            button2.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    if (onDialogCategoryResult != null) {
+                        KategorijaTransakcije kt = MyDatabase.getInstance(context).getKategorijaTransakcijeDAO().DohvatiKategorijuTransakcijePremaNazivu(kategorijaTransakcije.getNaziv());
+                        MyDatabase.getInstance(context).getKategorijaTransakcijeDAO().IzbrisiKategorijuTransakcije(kt);
+                        Toast.makeText(getContext(), "Izbrisana kategorija " + kt.getNaziv(), Toast.LENGTH_SHORT).show();
+                        onDialogCategoryResult.finish();
+                    }
+                    CategoryAddDialog.this.dismiss();
+                }
+            });
+        }
+
     }
     private void PostaviSpinner(){
         final String []tipovi=new String[]{"Prihod","Trošak","Prijenos"};
@@ -105,7 +135,7 @@ public class CategoryAddDialog extends Dialog implements View.OnClickListener {
         recyclerView.setAdapter(adapter);
     }
     private List<CategoryAddModel>VratiListuIkona(){
-        String ikone[] = {"ic_book","ic_fitness","ic_gaming","ic_kucni_ljubimci","ic_lijekovi","ic_obrazovanje","ic_pokloni","ic_travelling","ic_bank","ic_coffee","ic_growth","ic_money_transfer","ic_life_insurance"};
+
         arrayList = new ArrayList<>();
         for (int i = 0; i < ikone.length; i++) {
             CategoryAddModel categoryAddModel = new CategoryAddModel();
@@ -118,22 +148,54 @@ public class CategoryAddDialog extends Dialog implements View.OnClickListener {
 
         @Override
         public void onClick(View v) {
-            if(onDialogCategoryResult!=null){
-                if(!nazivKategorije.getText().toString().equals("")&&customAdapterKategorije.focusedItem>=0&&tipTroska>=0){
-                    Log.e("Kate",customAdapterKategorije.arrayList.get(customAdapterKategorije.focusedItem).getRawIkona());
-                    KategorijaTransakcije kt =new KategorijaTransakcije();
-                    kt.setNaziv(nazivKategorije.getText().toString());
-                    kt.setIkonaKategorije(customAdapterKategorije.arrayList.get(customAdapterKategorije.focusedItem).getRawIkona());
-                    kt.setTipTransakcije(tipTroska);
-                    MyDatabase.getInstance(getContext()).getKategorijaTransakcijeDAO().UnosKategorijeTransakcije(kt);
-                    Toast.makeText(getContext(),"Unesena nova kategorija "+kt.getNaziv(),Toast.LENGTH_SHORT).show();
+            if(kategorijaTransakcije==null) {
+                if (onDialogCategoryResult != null) {
+                    if (!nazivKategorije.getText().toString().equals("") && customAdapterKategorije.focusedItem >= 0 && tipTroska >= 0) {
+                        Log.e("Kate", customAdapterKategorije.arrayList.get(customAdapterKategorije.focusedItem).getRawIkona());
+                        KategorijaTransakcije kt = new KategorijaTransakcije();
+                        kt.setNaziv(nazivKategorije.getText().toString());
+                        kt.setIkonaKategorije(customAdapterKategorije.arrayList.get(customAdapterKategorije.focusedItem).getRawIkona());
+                        kt.setTipTransakcije(tipTroska);
+                        MyDatabase.getInstance(getContext()).getKategorijaTransakcijeDAO().UnosKategorijeTransakcije(kt);
+                        Toast.makeText(getContext(), "Unesena nova kategorija " + kt.getNaziv(), Toast.LENGTH_SHORT).show();
+                    } else {
+                        Toast.makeText(getContext(), "Niste unijeli sve parametre!", Toast.LENGTH_LONG).show();
+                    }
+                    onDialogCategoryResult.finish();
                 }
-                else{
-                    Toast.makeText(getContext(),"Niste unijeli sve parametre!",Toast.LENGTH_LONG).show();
-                }
-                onDialogCategoryResult.finish();
+                CategoryAddDialog.this.dismiss();
             }
-            CategoryAddDialog.this.dismiss();
+            else{
+                if (onDialogCategoryResult != null) {
+                    if (!nazivKategorije.getText().toString().equals("") && customAdapterKategorije.focusedItem >= 0 && tipTroska >= 0) {
+                        KategorijaTransakcije kt=MyDatabase.getInstance(context).getKategorijaTransakcijeDAO().DohvatiKategorijuTransakcijePremaNazivu(kategorijaTransakcije.getNaziv());
+                        kt.setId(kt.getId());
+                        kt.setNaziv(nazivKategorije.getText().toString());
+                        kt.setIkonaKategorije(customAdapterKategorije.arrayList.get(customAdapterKategorije.focusedItem).getRawIkona());
+                        kt.setTipTransakcije(tipTroska);
+                        MyDatabase.getInstance(getContext()).getKategorijaTransakcijeDAO().AzurirajKategorijuTransakcije(kt);
+                        Toast.makeText(getContext(), "Ažurirana kategorija " + kt.getNaziv(), Toast.LENGTH_SHORT).show();
+                    } else {
+                        Toast.makeText(getContext(), "Niste unijeli sve parametre!", Toast.LENGTH_LONG).show();
+                    }
+
+                    onDialogCategoryResult.finish();
+                }
+                CategoryAddDialog.this.dismiss();
+            }
+
         }
+    }
+    public void oznaciKategoriju(String nazivKategorije){
+        customAdapterKategorije.focusedItem=dohvatiIndexKategorije(nazivKategorije);
+    }
+    private int dohvatiIndexKategorije(String nazivKategorije){
+        int returnme=0;
+        for(int i=0;i<ikone.length;i++)
+            if(ikone[i].equals(nazivKategorije)){
+                returnme=i;
+                break;
+            }
+            return returnme;
     }
 }
