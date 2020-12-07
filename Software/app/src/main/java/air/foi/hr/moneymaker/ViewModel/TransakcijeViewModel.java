82a33ca784b3adf82cd1c1f13771cf07fd054974
dposavec <dposavec@foi.hr>
2020-12-07 19:24:17 +0100

    [AIR2007-65] Ažuriranje klasa TransakcijaFragment i TransakcijaViewModel.

--- Software/app/src/main/java/air/foi/hr/moneymaker/ViewModel/TransakcijeViewModel.java
+++ Software/app/src/main/java/air/foi/hr/moneymaker/ViewModel/TransakcijeViewModel.java
@@ -5,11 +5,15 @@ import android.view.MenuItem;
 
 import androidx.annotation.NonNull;
 import androidx.fragment.app.FragmentManager;
+import androidx.lifecycle.LiveData;
 import androidx.lifecycle.ViewModel;
 
 import com.google.android.material.bottomnavigation.BottomNavigationView;
 
+import java.util.List;
+
 import air.foi.hr.core.database.MyDatabase;
+import air.foi.hr.core.entiteti.Transakcija;
 import air.foi.hr.core.manager.FragmentName;
 import air.foi.hr.moneymaker.R;
 import air.foi.hr.moneymaker.manager.FragmentSwitcher;
@@ -18,6 +22,7 @@ public class TransakcijeViewModel extends ViewModel {
     private BottomNavigationView bottomNavigationView;
     private Context context;
     private MyDatabase baza;
+    public LiveData<List<Transakcija>> sveTransakcije;
 
     public void OznaciIndex(){
         bottomNavigationView.setSelectedItemId(R.id.transakcije);
@@ -52,5 +57,9 @@ public class TransakcijeViewModel extends ViewModel {
                 return true;
             }
         });
+    }
+    public LiveData<List<Transakcija>>VratiTransakcije(){
+        sveTransakcije=MyDatabase.getInstance(context).getTransakcijaDAO().DohvatiSveTransakcijeLIVE();
+        return sveTransakcije;
     }
 }
