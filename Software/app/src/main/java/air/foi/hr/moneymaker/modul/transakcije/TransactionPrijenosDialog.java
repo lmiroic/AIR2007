3ad7ffaaab75101bf391c646cc71ff8a2a744652
dposavec <dposavec@foi.hr>
2020-12-07 19:27:00 +0100

    [AIR2007-68] Kreirane i implementirane dialog klase. TransactionPrihodDialog, TransactionTrosakDialog i TransactionPrijenosDialog.

---
+++ Software/app/src/main/java/air/foi/hr/moneymaker/modul/transakcije/TransactionPrijenosDialog.java
@@ -0,0 +1,42 @@
+package air.foi.hr.moneymaker.modul.transakcije;
+
+import android.app.Dialog;
+import android.content.Context;
+import android.os.Bundle;
+import android.view.View;
+import android.view.Window;
+
+import androidx.annotation.NonNull;
+
+import air.foi.hr.core.modul.transakcije.OnDialogTransactionResult;
+import air.foi.hr.moneymaker.R;
+
+public class TransactionPrijenosDialog extends Dialog implements android.view.View.OnClickListener {
+
+    private OnDialogTransactionResult onDialogTransactionResult;
+
+    public TransactionPrijenosDialog(@NonNull Context context) {
+        super(context);
+    }
+    public void SetOnDialogTransactionResult(OnDialogTransactionResult onDialogTransactionResult){
+        this.onDialogTransactionResult=onDialogTransactionResult;
+    }
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        requestWindowFeature(Window.FEATURE_NO_TITLE);
+        setContentView(R.layout.dialog_prijenos);
+    }
+
+    @Override
+    public void onClick(View v) {
+
+    }
+    private class OkListener implements android.view.View.OnClickListener{
+        @Override
+        public void onClick(View v) {
+
+        }
+    }
+}
